4f358706f4020a6f59102ee070a6baf43e421970
afg360 <63133426+afg360@users.noreply.github.com>
2024-06-05 17:20:11 -0400

    Updated shell scripts to be able to initialise the different databases

--- scripts/README.md
+++ scripts/README.md
@@ -7,7 +7,19 @@ Make sure to set the appropriate variables before running the scripts, depending
 ## Windows
 If you are using Windows, enable permission to run powershell scripts. You can then run the script by running `./initall.ps1` inside powershell
 You may need to change the $path and $project folder names to something else (on lines 42 and 47 respectively)
-
+### Syntax
+To initialise all the databases, run this command:
+```powershell
+./initall.ps1
+```
+If you only need it for a specific agency, run:
+```powershell
+./initall.ps1 <agency-name>
+```
+You can also check the help output by running:
+```powershell
+./initall.ps1 -help
+```
 ## Linux
 The script for linux should be ran under bash. Be sure to give the right permissions to the file so that it can be run:
 ```bash
@@ -17,6 +29,8 @@ chmod u+x initall.sh
 If you use the script without needing to download the txt files, make sure to set the right paths by line 44 for the $path variable.
 Moreover, do the same regardless for the download, to the $project folder, which would point to folder in the android project where the
 assets will be stored (in this case the database).
+
+If you want to use the exo command, you would need to replace the $path variable to your proper path, at line 103.
 ### Dependencies
 The script uses curl to download the necessary files, as well as unzip in linux for unziping the zip file. You must have both installed before
 proceeding.
--- scripts/initall.sh
+++ scripts/initall.sh
@@ -1,88 +1,116 @@
 #!/bin/bash
 
-#check if the txt files exist (assuming they are the correct ones)
-list=$(find . -type f -name "*.txt" | grep -v 'note[s]\?.txt')
+if [[ $1 = "--help" ]]; then
+  echo "Script to initialise databases containing static data from transit agencies.
+      Usage: $ ./initall.sh <agency-name> -> download the data and initialise the database
+                                              only related to the given agency-name.
+             $ ./initall.sh -> download the data from all the agencies and init all the databases.
+             $ ./initall.sh --help -> print this help message
+      E.g.:  $ ./initall.sh stm
+      "
 
-#download them if the user chooses to. Curl and unzip must be installed to proceed
-if [ -z "$list" ]; then
-    echo "No matching txt data files found in the subdirectories."
-        echo -n "Do you wish to download them now? [y/n] (no will abort the script) -> "
-        read -r answer
-        if [[ $answer = "y" || $answer = "yes" ]]; then
-            if ! command unzip &> /dev/null; then
-                echo "unzip is not installed on this system. You must install it before running this script. Aborting."
-                exit 2
-            fi
-            curl -L https://www.stm.info/sites/default/files/gtfs/gtfs_stm.zip -o data.zip
-            unzip data.zip
-            if [[ $? -eq 0 ]]; then
-                rm agency.txt calendar_dates.txt feed_info.txt data.zip
-            else
-                echo "An error occured trying to unzip the data file. Aborting the script"
-                exit $?
-            fi
-            #move the unzip files to the correct directory, named similarly
-            for file in *.txt; do
-                if [[ -f $file ]]; then
-                    if [[ $file = "stop_times.txt" ]]; then
-                        mv "$file" "./sql/stop-times"
-                    else
-                            base_name="./sql/${file%.txt}"
-                        if [[ -d $base_name ]]; then
-                            mv "$file" "$base_name/"
-                        fi
-                    fi
-                fi
-            done
-            path=$(pwd)/sql
-        else
-            echo "Aborting script"
-            exit 1
-        fi
-else
-    #The below could look like path=$PROJECT_FOLDER/scripts/sql
-    path=/absolute/path/to/scripts/folder/scripts/sql
-fi
+#stm download
+elif [[ $1 = "stm" ]]; then
+  #check if the txt files exist (assuming they are the correct ones)
+  list=$(find . -type f -name "*.txt" | grep -v 'note[s]\?.txt')
 
-#initialisation of the database starts here
-
-#The below should point to the folder where you want to place
-#the database inside your project
-#e.g. project=$PROJECT_FOLDER/src/main/assets/database
-project=/absolute/path/to/your/project/database/folder
-
-cd $path
-for dir in $(ls)
-do
-  if [[ ! -d $dir ]]; then
-    continue
+  #download them if the user chooses to. Curl and unzip must be installed to proceed
+  if [ -z "$list" ]; then
+      echo "No matching txt data files found in the subdirectories."
+          echo -n "Do you wish to download them now? [y/n] (no will abort the script) -> "
+          read -r answer
+          if [[ $answer = "y" || $answer = "yes" ]]; then
+              if ! command unzip &> /dev/null; then
+                  echo "unzip is not installed on this system. You must install it before running this script. Aborting."
+                  exit 2
+              fi
+              curl -L https://www.stm.info/sites/default/files/gtfs/gtfs_stm.zip -o data.zip
+              unzip data.zip
+              if [[ $? -eq 0 ]]; then
+                  rm agency.txt calendar_dates.txt feed_info.txt data.zip
+              else
+                  echo "An error occured trying to unzip the data file. Aborting the script"
+                  exit $?
+              fi
+              #move the unzip files to the correct directory, named similarly
+              for file in *.txt; do
+                  if [[ -f $file ]]; then
+                      if [[ $file = "stop_times.txt" ]]; then
+                          mv "$file" "./stm/stop-times"
+                      else
+                              base_name="./stm/${file%.txt}"
+                          if [[ -d $base_name ]]; then
+                              mv "$file" "$base_name/"
+                          fi
+                      fi
+                  fi
+              done
+              path=$(pwd)/stm
+          else
+              echo "Aborting script"
+              exit 1
+          fi
+  else
+      #The below could look like path=$PROJECT_FOLDER/scripts/sql
+      path=/absolute/path/to/scripts/folder/scripts/sql
   fi
-	cd $dir
-	echo "Working on $dir"
-	echo "Starting script.py"
-	python3 script.py
-	#check if python script executed properly
-	if [[ $? -ne 0 ]]; then
-		echo -e "Skipping $dir. No python script to run in this folder\n\n"
-	else
-		echo -e "Done\n\n"
-	fi
-	cd ..
-done
 
-cd $path/..
-#run the python script to create the new table containing better info than stoptimes
-python3 ./script.py
-echo "Completed the initialisation of the database"
+  #initialisation of the database starts here
 
-#copy to the $project path if the user chooses to
-echo -n "Would you like to copy the database for the android schedules project? [y/n] -> "
-read choice
-if [[ $choice = 'y' || 'yes' ]]; then
-    echo "Copying database to the project"
-	cp -i 'sql/stm_info.db' $project
+  #The below should point to the folder where you want to place
+  #the database inside your project
+  #e.g. project=$PROJECT_FOLDER/src/main/assets/database
+  project=/absolute/path/to/your/project/database/folder
+
+  cd $path
+  for dir in $(ls)
+  do
+    if [[ ! -d $dir ]]; then
+      continue
+    fi
+  	cd $dir
+  	echo "Working on $dir"
+  	echo "Starting script.py"
+  	python3 script.py
+  	#check if python script executed properly
+  	if [[ $? -ne 0 ]]; then
+  		echo -e "Skipping $dir. No python script to run in this folder\n\n"
+  	else
+  		echo -e "Done\n\n"
+  	fi
+  	cd ..
+  done
+
+  cd $path/..
+  #run the python script to create the new table containing better info than stoptimes
+  python3 ./script.py
+  echo "Completed the initialisation of the database"
+
+  #copy to the $project path if the user chooses to
+  echo -n "Would you like to copy the database for the android schedules project? [y/n] -> "
+  read choice
+  if [[ $choice = 'y' || 'yes' ]]; then
+      echo "Copying database to the project"
+  	cp -i 'stm/stm_info.db' $project
+  else
+  	echo "No copying. Don't forget to do the process manually"
+  fi
+  echo "Completed task"
+  exit 0
+
+elif [[ $1 = "exo" ]]; then
+  #to replace below if needed
+  #path=/project/folder
+  path=.
+  cd $path/exo
+  python3 setup_exo_db.py
+  exit $?
+
+#all downloads
+elif [[ $# -eq 0 ]]; then
+  echo "Not implemented yet..."
+
 else
-	echo "No copying. Don't forget to do the process manually"
+  echo "Syntax Error!"
+  exit 1
 fi
-echo "Completed task"
-exit 0
